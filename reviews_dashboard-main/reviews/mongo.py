05bfb66385280625b6ae51c46ebe3ca16fef6ba0
IMAD EL ANNASI <elannasiimad@gmail.com>
2025-05-23 00:11:50 +0100

    Merge Commit

--- reviews_dashboard-main/reviews/mongo.py
+++
@@ -1,8 +0,0 @@
-# reviews/mongo.py
-from django.conf import settings
-from pymongo import MongoClient
-
-# settings.MONGO_HOST will default to "mongo" (the service name in docker-compose)
-client     = MongoClient(settings.MONGO_HOST, settings.MONGO_PORT)
-db         = client["amazon"]
-collection = db["reviews"]
